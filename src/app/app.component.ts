f5f1304e9eecf973990e3b37e57e2d7ed3603f9a
DelaneyGalvanize <delaney.patterson001@gmail.com>
2017-07-13 13:03:43 -0600

    assignment one complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles:[` h1 {
+  //   color:red;
+  // }`]
 })
 export class AppComponent {
   title = 'app';

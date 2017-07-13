51ab0d25e8481763a5bda6a1c75c3ae179c7985b
DelaneyGalvanize <delaney.patterson001@gmail.com>
2017-07-13 14:09:56 -0600

    data binding practice

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,12 @@ import {Component} from '@angular/core'
 templateUrl: './server.component.html',
 })
 export class ServerComponent {
+  serverID = 10;
+  serverStatus= 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus= 'no server was created'
+  serverName='testServer';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+   }
 
   ngOnInit() {
   }
 
+  onCreateServer () {
+    this.serverCreationStatus= 'server was created Name is ' +this.serverName;
+  }
+
+  onUpdateServerName(event:Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+
+  }
 }
